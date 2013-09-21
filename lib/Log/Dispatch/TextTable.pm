3fde7eb8c8489b7f3076ba937da2ba25f941013a
Ricardo Signes <rjbs@cpan.org>
2013-09-21 15:56:24 +0900

    dzilify

--- lib/Log/Dispatch/TextTable.pm
+++ lib/Log/Dispatch/TextTable.pm
@@ -1,24 +1,11 @@
-package Log::Dispatch::TextTable;
-use base qw(Log::Dispatch::Output);
-
-use warnings;
 use strict;
+use warnings;
+package Log::Dispatch::TextTable;
+use parent qw(Log::Dispatch::Output);
+# ABSTRACT: log events to a textual table
 
+use Log::Dispatch 2.0 ();
 use Text::Table;
-
-=head1 NAME
-
-Log::Dispatch::TextTable - log events to a textual table
-
-=head1 VERSION
-
-version 0.031
-
- $Id$
-
-=cut
-
-our $VERSION = '0.031';
 
 =head1 SYNOPSIS
 
@@ -44,9 +31,7 @@ This provides a Log::Dispatch log output system that builds logged events into
 a textual table and, when done, does something with the table.  By default, it
 will print the table.
 
-=head1 METHODS
-
-=head2 C<< new >>
+=method new
 
  my $table_log = Log::Dispatch::TextTable->new(\%arg);
 
@@ -67,7 +52,7 @@ sub new {
 
   # when done, by default, print out the passed-in table
   $arg{send_to} ||= sub { print $_[0] };
-  
+
   # construct the column list, using the default if no columns were given
   my @columns = $arg{columns} ? @{ $arg{columns} } : qw(time level message);
   my @header  = map { $_, \q{ | } } @columns;
@@ -81,7 +66,7 @@ sub new {
     send_to  => $arg{send_to},
     flush_if => $arg{flush_if},
   };
-  
+
   bless $self => $class;
 
   # this is our duty as a well-behaved Log::Dispatch plugin
@@ -90,7 +75,7 @@ sub new {
   return $self;
 }
 
-=head2 C<< log_message >>
+=method log_message
 
 This is the method which performs the actual logging, as detailed by
 Log::Dispatch::Output.  It adds the data to the table and may flush.  (See
@@ -109,7 +94,7 @@ sub log_message {
   $self->flush(\%p) if $self->should_flush;
 }
 
-=head2 C<< table >>
+=method table
 
 This method returns the Text::Table object being used for the log's logging.
 
@@ -117,7 +102,7 @@ This method returns the Text::Table object being used for the log's logging.
 
 sub table { return $_[0]->{table} }
 
-=head2 C<< entry_count >>
+=method entry_count
 
 This method returns the current number of entries in the table.
 
@@ -128,7 +113,7 @@ sub entry_count {
   $self->table->body_height;
 }
 
-=head2 C<< flush >>
+=method flush
 
 This method transmits the current table and then clears it.  This is useful for
 emptying large tables every now and then.
@@ -141,7 +126,7 @@ sub flush {
   $self->table->clear;
 }
 
-=head2 C<< should_flush >>
+=method should_flush
 
 This method returns true if the logger is ready to flush its contents.  This is
 always false, unless a C<flush_if> callback was provided during instantiation.
@@ -159,7 +144,7 @@ sub should_flush {
   return $self->{flush_if}->($self, $p);
 }
 
-=head2 C<< transmit >>
+=method transmit
 
 This method sends out the table's current contents to their destination via
 the callback provided via the C<send_to> argument to C<new>.
@@ -176,28 +161,10 @@ sub DESTROY {
   $self->transmit;
 }
 
-=head1 AUTHOR
-
-Ricardo Signes, C<< <rjbs@cpan.org> >>
-
 =head1 TODO
 
 I'd like to make it possible to transmit just the rows since the last transmit
 I<without> flushing, but Text::Table needs a bit of a patch for that.
-
-=head1 BUGS
-
-Please report any bugs or feature requests to
-C<bug-log-dispatch-table@rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org>.  I will be notified, and then you'll automatically be
-notified of progress on your bug as I make changes.
-
-=head1 COPYRIGHT
-
-Copyright 2005-2006 Ricardo SIGNES, All Rights Reserved.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
 
 =cut
 
--- t/00.load.t
+++
@@ -1,7 +0,0 @@
-use Test::More tests => 1;
-
-BEGIN {
-  use_ok('Log::Dispatch::TextTable');
-}
-
-diag( "Testing Log::Dispatch::TextTable $Log::Dispatch::TextTable::VERSION" );
